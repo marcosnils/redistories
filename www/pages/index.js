ee626ed96b420ad42676a1bd82d19625c10924c6
Matheus Fernandes <matheus.frndes@gmail.com>
2019-11-12 07:58:05 +0000

    Poll every 500ms

--- www/pages/index.js
+++ www/pages/index.js
@@ -3,12 +3,12 @@ import fetch from 'isomorphic-unfetch';
 import React, { Component } from 'react'
 
 export default class extends Component {
-constructor() {
-      super();
-      this.state = {
-        messages: [],
-      };
-    }
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
 
   render() {
     return (
@@ -26,9 +26,17 @@ constructor() {
   }
 
   async componentDidMount() {
-  const res = await fetch('/api/stories');
-  const messages = await res.json();
-  this.setState({...this.state, messages});
+    const update = async () => {
+      const res = await fetch('/api/stories');
+      const messages = await res.json();
+      this.setState({...this.state, messages});
+      this.timeout = setTimeout(update, 250);
+    }
+    this.timeout = setTimeout(update, 0);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
   }
 }
 
